951d066f3f355cfb4ac87d4509792fdfe8a2aa82
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2024-02-05 00:39:10 +0600

    Optimised

--- js-string-tasks-main/task5.js
+++ js-string-tasks-main/task5.js
@@ -1,4 +1,4 @@
-const str = "my name is anupam roy";
+const str = "i am learning web development";
 let result = "";
 console.log(str.length);
 
@@ -6,17 +6,13 @@ for (let i = 0; i < str.length; i++) {
     if (i === 0) {
         result = result + str[i].toUpperCase();
     }
-    if (str[i] === ' ' && str[i+1] !== ' ' && i !== str.length-1) {
-        if (i !== 0) {
-            result = result + str[i];
-        }
+    else if (str[i] === ' ' && str[i + 1] !== ' ' && i !== str.length - 1) {
+        result = result + str[i];
         result = result + str[i + 1].toUpperCase();
         i++;
     }
     else {
-        if (i !== 0) {
-            result = result + str[i];
-        }
+        result = result + str[i].toLowerCase();
     }
 }
 
